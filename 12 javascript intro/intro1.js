cc58f87791c51edd391c3853ca4a809932dd8df3
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-03 11:52:07 +0530

    a

--- 12 javascript intro/intro1.js
+++ 12 javascript intro/intro1.js
@@ -1,25 +1,159 @@
 // this is just practice means it is not link with any file
 // and also it it not in proper javascript way
 
+
+// JAVASCRIPT TYPES
+// -----------------
+// 1. Number 
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// <!-- 6. Symbol (new in ECMAScript 6) -->
+// 7. Object
+
+// JAVASCRIPT COMPARISONS
+// -----------------
+
+// !==
+// ===
+// >=
+// <=
+// >
+// <
+
+// JAVASCRIPT VARIABLES
+// -----------------
+// var
+// <!-- let (new in ECMAScript 6)-->  
+// <!-- const (new in ECMAScript 6)-->
+
+// JAVASCRIPT CONDITIONALS
+// -----------------
+// if
+// else
+// else if
+// <!-- ternary operator -->
+// <!-- switch -->
+
+
+// JAVASCRIPT LOGICAL OPERATORS
+// -----------------
+// &&
+// ||
+// !
+
+// JAVASCRIPT FUNCTIONS
+// -----------------
+// var a = function name() {}
+// function name() {}
+// return
+// <!-- () => (new in ECMAScript 6) -->
+
+// JAVASCRIPT DATA STRUCTURES
+// -----------------
+// Array
+// Object
+
+// JAVASCRIPT LOOPING
+// -----------------
+// for
+// while
+// do 
+// forEach (new in ECMAScript 5) 
+
+
+// JAVASCRIPT KEYWORDS
+// -----------------
+// break
+// case
+// catch
+// class
+// const
+// continue
+// debugger
+// default
+// delete
+// do
+// else
+// export
+// extends
+// finally
+// for
+// function
+// if
+// import
+// in
+// instanceof
+// new
+// return
+// super
+// switch
+// this
+// throw
+// try
+// typeof
+// var
+// void
+// while
+// with
+// yield
+
+// -----------------------------------------------------------------------------------------------------------------------
+
+// ****rules****
+
+// STRING
+// you can create string by the doublequots ("") or the singlequots ('')
+
+
 5 + "34" // "534"
+// here the 5 is an integer and 34 is a string 
+// when you add a integer with the string the javascript act like you want to add two strings
+// it is not mistake it is the neature of the javascript
+
 5 - "4" // 1
+// but when tries you substract the integer by an string it actually substrqact the way of an integer substractor
+// it happens because the strings can never substracted
+// only integer can be substracted
+// hence here the javascript automatically converts the strings into the integer 
+
 10 % 5 // 0
+// % its a sign of a modula it always give the output in the reminder
+
 5 % 10 // 5
+
 "Java" + "Script" // "JavaScript"
+// you can mearge the two strings by using the plus (+) sign
+
 " " + " " //  "  "
 " " + 0 //  " 0"
+// when you mearging the int with the string it converts the int into the string automatically
+
 true + true // 2
+// true and false are the boolen values
+
 true + false // 1
 false + true //  1
 false - true //  -1
-3 - 4 //  -1
-"Bob" - "bill" // NAN
 
+3 - 4 //  -1
+
+"Bob" - "bill" // NAN
+// when you tries to substract the string to the string it gives the NaN
+// NaN means not a number
 
 5 >= 1 // true
+// these are the javasvript comparison
+
 0 === 1 //  false
+// when you comparing the number you have to compare with three equal signs ( === )
+
 4 <= 1 //  false
+
 1 != 1 //  false
+// this is the not sing (!)
+
 "A" > "B" //  false
 "B" < "C" //  true
 "a" > "A" //  true
