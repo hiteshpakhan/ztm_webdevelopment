532ec43eb35f66513cc8eb3e7db2c5f14642ae91
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-07 13:07:56 +0530

    a

--- 13 Javascript - Part02/JAVASCRIPT_DATA_STRUCTURES.js
+++ 13 Javascript - Part02/JAVASCRIPT_DATA_STRUCTURES.js
@@ -49,5 +49,28 @@
 // valueOf()	    Returns the primitive value of an array
 
 
+// some array examples:-
+// using this array, 
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
-// go to the folder array for explanation
+// 1. Remove the Banana from the array.
+array.shift();
+
+// 2. Sort the array in order. 
+array.sort();
+
+// 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
+
+// 4. Remove "Apples" from the array.
+array.splice(0, 1);
+
+// 5. Sort the array in reverse order. 
+array.reverse();
+
+// using this array, 
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+array2[1][1][0];
+
+// go to the folder array for more explanation
