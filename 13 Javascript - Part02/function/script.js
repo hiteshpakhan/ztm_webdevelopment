5a66917da0771004e6b819cd44f0eab431ec3b34
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-11 11:34:19 +0530

    a

--- 13 Javascript - Part02/function/script.js
+++ 13 Javascript - Part02/function/script.js
@@ -1,7 +1,25 @@
+// TYPE 1: function declaration 
+// syntax:  function_name(){}
+
+function say_hello(){
+    console.log("hello");
+}
+say_hello();
+
+// it will give the output:-
+// hello
 
 
 
+// TYPE 2: function expression
+// syntax: var variable_name = function () {}
 
+var say_bye = function(){
+    console.log("BYE");
+}
+say_bye();
+// it will give the output:-
+// BYE
 
 
 
