b4a837c4a14a3923f930563cbf3952a7ebad4a92
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-12 20:12:13 +0530

    a

---
+++ 13 Javascript - Part02/small login exercise/script.js
@@ -0,0 +1,28 @@
+var database = [
+    {
+        username : "name_1",
+        password : "pass_1"
+    },
+    {
+        username : "name_2",
+        password : "pass_2"
+    }
+];
+
+var newsFeed = [
+    {
+        username : "boby",
+        timeline : "enjoying the beach"
+    },
+    {
+        username : "sally",
+        timeline : "javascript is so cool"
+    }
+];
+
+var given_username = prompt("enter the username : ");
+var given_password = prompt("enter your pass ");
+
+
+
+
