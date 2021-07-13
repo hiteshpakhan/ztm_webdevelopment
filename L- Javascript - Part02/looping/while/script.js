9b6f0806c6faf9dceee6ba267422c16be982e9e3
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-13 23:31:43 +0530

    a

---
+++ L- Javascript - Part02/looping/while/script.js
@@ -0,0 +1,53 @@
+// while loop
+// example:-
+var counter1 = 0;
+while(counter1<10){
+    console.log(counter1);
+    counter1++;
+}
+// it will give us the output:-
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// it stops because we gave it condition (counter1<10)
+
+var counter2 = 10;
+while(counter2>0){
+    console.log(counter2);
+    counter2--;
+}
+// it will give you the answere :-
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// it will give the same in reverse mode
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
