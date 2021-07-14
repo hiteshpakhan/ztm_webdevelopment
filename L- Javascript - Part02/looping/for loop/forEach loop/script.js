1de41a5b7fb85ddfecbe5bdea355951c051714f8
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-14 21:25:31 +0530

    a

--- L- Javascript - Part02/looping/for loop/forEach loop/script.js
+++ L- Javascript - Part02/looping/for loop/forEach loop/script.js
@@ -19,6 +19,37 @@ list_arr1.forEach(function(i){
 // item_3
 // item_4
 // item_5
+// 
+// there is the one another way 
+// 
+list_arr1.forEach(function(list_arr1){
+    console.log(list_arr1);
+});
+// even if i replace the i with the list_arr1 it gives us the same output
+//  item_1
+//  item_2
+//  item_3
+//  item_4
+//  item_5
+
+
+
+list_arr1.forEach(function(list_arr1, i){
+    console.log(list_arr1, i);
+});
+// it will give you the output
+// item_1 0
+// item_2 1
+// item_3 2
+// item_4 3
+// item_5 4
+
+
+
+
+
+
+
 
 
 
