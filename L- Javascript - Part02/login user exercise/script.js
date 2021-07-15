501803d032a2eaf0e563cdc988c725fce559d1c0
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-15 23:51:41 +0530

    a

--- L- Javascript - Part02/login user exercise/script.js
+++ L- Javascript - Part02/login user exercise/script.js
@@ -21,7 +21,9 @@ var get_pass = prompt("enter your pass ");
 
 function sign_in(user, pass){
     if(user === database[0].username && pass === database[0].password){
+        // here database is the array [0] is the first item of an array but its an object so we further joined the .username to access the username inside the object
         console.log(newsFeed);
+        // it just the access to the newsFeed 
     }else{
         alert("sorry wrong username or password");
     }
