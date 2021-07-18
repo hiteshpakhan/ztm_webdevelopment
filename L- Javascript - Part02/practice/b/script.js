abf6fd7634af138bb5df859cdb5ad3164b764df9
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-07-18 19:40:25 +0530

    a

--- L- Javascript - Part02/practice/b/script.js
+++ L- Javascript - Part02/practice/b/script.js
@@ -17,16 +17,16 @@ var database = [
     }
 ];
 
-var user = prompt("enter your username");
-var pass = prompt("enter your password");
+var user = prompt("enter your username");      //it will take the input for the username
+var pass = prompt("enter your password");      //it will take the input for the password
 
-function fun2(a, b){
-    for (var i=0; i<database.length; i++){
+function fun2(a, b){                           //we are giving the a and b the values of username and password 
+    for (var i=0; i<database.length; i++){      
         if(a===database[i].username && b===database[i].password){
-            return true;
+            return true;                       //the moment it wil gets the value of return it will stops the execution and get out of the (if) and the (for) statement
         }
     }
-    return false;
+    return false;                              //and the value of the username and password dosent mach the any of the values in the database it will return the value false 
 }
 
 function login(a, b){
