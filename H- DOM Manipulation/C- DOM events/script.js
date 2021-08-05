c5f895b9826e6ec48016a4626d10cd2bfd89b957
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-05 18:21:28 +0530

    a

--- H- DOM Manipulation/C- DOM events/script.js
+++ H- DOM Manipulation/C- DOM events/script.js
@@ -4,8 +4,14 @@ var demo = document.getElementsByTagName("button")[0];
 
 demo.addEventListener("click", function(){    //here whenever the click happen the function will execute
     // addEventListener used to access the events
+    // click is the event listener that react when you click on somthing
     console.log("click action perform");
 })
+
+// there are other mouse event listener for example:-
+// mouseenter : it react when you hover the mouse to it
+// mouseleave : it react as soon as you leave the mouse from there
+// there are many of the event listener    https://www.w3schools.com/jsref/dom_obj_event.asp
 
 
 
