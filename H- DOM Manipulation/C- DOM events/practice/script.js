9c6edf4d92a4e6d7981567245f385f3b9d45e379
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-06 16:18:11 +0530

    a

---
+++ H- DOM Manipulation/C- DOM events/practice/script.js
@@ -0,0 +1,16 @@
+var item_name = document.getElementById("user_input");
+var btn = document.getElementById("enter");
+
+btn.addEventListener("click",function(){
+    console.log("click is working");
+})
+
+
+
+
+
+
+
+
+
+
