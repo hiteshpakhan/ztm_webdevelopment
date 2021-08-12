8857183b1f80bb83c0f709a31024ecd54ba5b541
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-12 13:22:02 +0530

    a

--- H- DOM Manipulation/C- DOM events/practice/script.js
+++ H- DOM Manipulation/C- DOM events/practice/script.js
@@ -1,7 +1,7 @@
 var item_name = document.getElementById("id_input");
 var btn = document.getElementById("id_enter");
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click",function(){     //here we add the event listener to the button 
     console.log("click is working");
 })
 
