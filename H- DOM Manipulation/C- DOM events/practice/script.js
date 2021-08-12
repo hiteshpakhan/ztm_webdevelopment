991d812639c0a4ec7a9f19fe8c559cd87b5e1883
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-12 17:30:46 +0530

    a

--- H- DOM Manipulation/C- DOM events/practice/script.js
+++ H- DOM Manipulation/C- DOM events/practice/script.js
@@ -1,8 +1,11 @@
-var item_name = document.getElementById("id_input");
-var btn = document.getElementById("id_enter");
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector(ul);
 
-btn.addEventListener("click",function(){     //here we add the event listener to the button 
-    console.log("click is working");
+button.addEventListener("click",function(){     //here we add the event listener to the button 
+    var li = document.createElement("li");   //here we create the new element li
+    li.appendChild(document.createTextNode("testing"));  //here we add the some text to the li element
+    ul.appendChild(li);
 })
 
 
