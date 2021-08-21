57adaed41311f38c26962e6b5592f91240cdba4d
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-21 13:59:35 +0530

    a

--- H- DOM Manipulation/C- DOM events/practice/script.js
+++ H- DOM Manipulation/C- DOM events/practice/script.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector(ul);
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {     
     var li = document.createElement("li");   
