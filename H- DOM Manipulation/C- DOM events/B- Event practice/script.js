44ce5fc160406533d2091f052feff861992744c2
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-26 09:37:02 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -1,13 +1,13 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var ul = document.querySelector("ul");   
 
-button.addEventListener("click", function(){
-    if(input.value.length > 0){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+button.addEventListener("click", function(){   //this function will exeacute if you click the button
+    if(input.value.length > 0){   // this will check the input text length
+        var li = document.createElement("li");   //this will create the new li tag
+        li.appendChild(document.createTextNode(input.value));  //this will create the text in li tag
+        ul.appendChild(li);   //this will make li the child of ul
+        input.value = "";   //this will make the input box empty
     }
 } )
 
