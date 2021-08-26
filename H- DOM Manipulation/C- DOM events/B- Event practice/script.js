895b893d6c02c91b06b1c6e4c69c4e4a80e337b5
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-26 09:31:32 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -3,9 +3,12 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
+    if(input.value.length > 0){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
 } )
 
 
