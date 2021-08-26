cbb0a705e9b5d8d186dd137dc26d1c0c878bbe30
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-26 09:29:26 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -3,7 +3,9 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function(){
-    
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
 } )
 
 
