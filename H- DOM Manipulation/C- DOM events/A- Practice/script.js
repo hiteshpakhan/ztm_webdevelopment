c8020f20bd0bff012fc5b55bdcd37d0f40ecdff4
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-30 21:11:34 +0530

    a

--- H- DOM Manipulation/C- DOM events/A- Practice/script.js
+++ H- DOM Manipulation/C- DOM events/A- Practice/script.js
@@ -7,13 +7,14 @@ if(inside === "Hello Everyone"){
 
 document.querySelector("input").click();   //it will on the click 
 document.querySelector("input").click();   //it will off the click
+document.querySelector("input").click();   //it will on the click 
 // -----
 
 document.getElementsByTagName("li")[0].style.color="red";   //it will set the color red of the frist li element  
 // -----
 
-var countitem = document.getElementsByTagName("li").length;
-document.querySelector("p").innerHTML = "there are "+countitem+" items in the li tag";
+var countitem = document.getElementsByTagName("li").length;    //it will count the no of length of li element
+document.querySelector("p").innerHTML = "there are "+countitem+" items in the li tag";   
 // -----
 
 
