7877d1c6e3127ccac5616ccbee056ecd61fcb8c8
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-08-31 23:11:51 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -14,7 +14,6 @@ button.addEventListener("click", function(){   //this function will exeacute if 
 // it can also be added by clicking the enter button on keybord 
 // for that we have to use the following programe
 
-
 input.addEventListener("keypress", function(event){   //this function will exeacute if you click the button
     if(input.value.length > 0 && event.keyCode === 13){   // this will check the input text length
         var li = document.createElement("li");   //this will create the new li tag
