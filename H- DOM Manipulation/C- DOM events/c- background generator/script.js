9192c883d769438ecb2072f78f5350a19f3311a3
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-04 05:01:49 +0530

    a

--- H- DOM Manipulation/C- DOM events/c- background generator/script.js
+++ H- DOM Manipulation/C- DOM events/c- background generator/script.js
@@ -1,7 +1,10 @@
-var css = dociment.querySelector("h3");
-var color1 = document.querySelector("c1")
-var color2 = document.querySelector("c2")
+var css = document.querySelector("h3");
+var color1 = document.querySelector("c1");
+var color2 = document.querySelector("c2");
 
+console.log(css);
+console.log(color1);
+console.log(color2);
 
 
 
