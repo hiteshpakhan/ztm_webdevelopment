0c436ca4a8113516c315ad36e1759ae517220e88
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-04 05:21:51 +0530

    a

--- H- DOM Manipulation/C- DOM events/c- background generator/script.js
+++ H- DOM Manipulation/C- DOM events/c- background generator/script.js
@@ -2,11 +2,11 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".c1");
 var color2 = document.querySelector(".c2");
 var body = document.getElementById("back");
-console.log(body);
 
 color1.addEventListener("input", function(){
     console.log(color1.value);
 });
+
 color2.addEventListener("input", function(){
     console.log(color2.value);
 });
