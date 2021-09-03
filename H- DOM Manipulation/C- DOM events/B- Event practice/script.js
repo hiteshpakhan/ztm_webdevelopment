138c5f39d35d63cdbad74678a5b86dba984a37af
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-03 06:43:37 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -15,7 +15,7 @@ button.addEventListener("click", function(){   //this function will exeacute if 
 // for that we have to use the following programe
 
 input.addEventListener("keypress", function(event){   //this function will exeacute if you click the button
-    if(input.value.length > 0 && event.keyCode === 13){   // this will check the input text length
+    if(input.value.length > 0 && event.keyCode === 13){   // this will check the input text length    //in the keyCode the letter C is capital
         var li = document.createElement("li");   //this will create the new li tag
         li.appendChild(document.createTextNode(input.value));  //this will create the text in li tag
         ul.appendChild(li);   //this will make li the child of ul
