c6a872d930a7a73d65a2c7a4f129f77d9bed5cfb
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-03 07:09:53 +0530

    a

--- H- DOM Manipulation/C- DOM events/B- Event practice/script.js
+++ H- DOM Manipulation/C- DOM events/B- Event practice/script.js
@@ -38,7 +38,16 @@ button.addEventListener("click", function(){
         ul.appendChild(li);    
         input.value = ""; 
     }
-})
+});
+
+input.addEventListener("keypress", function(event){   
+    if(a() > 0 && event.keyCode === 13){   
+        var li = document.createElement("li");  
+        li.appendChild(document.createTextNode(input.value)); 
+        ul.appendChild(li);  
+        input.value = "";   
+    }
+});
 
 
 
