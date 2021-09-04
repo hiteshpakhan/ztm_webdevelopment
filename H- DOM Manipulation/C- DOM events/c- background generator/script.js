5f5b7bdfea2b80ef7c5fe992860ff3fdd51da9f7
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-04 05:42:14 +0530

    a

--- H- DOM Manipulation/C- DOM events/c- background generator/script.js
+++ H- DOM Manipulation/C- DOM events/c- background generator/script.js
@@ -1,11 +1,16 @@
 var css = document.querySelector("h3");
-var color1 = document.querySelector(".c1");
-var color2 = document.querySelector(".c2");
-var body = document.getElementById("back");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
 color1.addEventListener("input", function(){
-    console.log(color1.value);
-});
+    body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+})
 
 color2.addEventListener("input", function(){
     console.log(color2.value);
