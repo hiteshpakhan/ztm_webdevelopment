f191a3eb4f65a24e5776c69a3a66ef04ac9f48f8
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 06:31:11 +0530

    a

---
+++ I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
@@ -0,0 +1,10 @@
+// # forEach
+
+    const arr = [1,2,3,4,5];
+    const newarr = arr.forEach((num) => {
+        num * 2;
+    })
+    console.log(newarr);
+
+    // output:-
+    // [2, 4, 6, 8, 10]
