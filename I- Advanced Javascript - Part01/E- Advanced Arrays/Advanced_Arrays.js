1f0e4c1d904b35847f46cb41e17102664a7a2e8c
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 08:30:07 +0530

    a

--- I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
+++ I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
@@ -25,3 +25,11 @@
     // output:-
     // [2, 4, 6, 8, 10]      //the same output as forEach but with map we dont have to assign the values sepreatly
 
+
+// filter
+
+const arr = [1,2,3,4,5];
+const newarr = arr.filter(num => {
+    return num > 2;
+})   //also you can do this in the short form:- const newarr = arr.filter(num => num > 2);
+console.log(newarr);
