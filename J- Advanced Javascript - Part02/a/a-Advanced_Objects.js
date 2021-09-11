e218298c73fbbde826e16b9fe5332245c5868945
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 16:54:19 +0530

    a

--- J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
+++ J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
@@ -1,7 +1,8 @@
 // reference type
 
 const object1 = { value: 10 };  //object is created
-const object2 = object1;        //
+const object2 = object1;        // here the main thing is
+//object2 got not assign the values of the boject1 its just referes the values of object
 const object3 = object2; 
 const object4 = { value: 10};  
 
