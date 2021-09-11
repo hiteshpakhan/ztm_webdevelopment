4072d9499baa47449aee7c9342b05491f22300cb
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 08:14:16 +0530

    a

--- I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
+++ I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
@@ -18,10 +18,10 @@
     const arr = [1,2,3,4,5];
     const newarr = arr.map((num) => {
         return num * 2;
+        //you can also write in different way:-     const newarr = newarr.map(num => num * 2);  
     })
     console.log(newarr);
 
     // output:-
-    // [2, 4, 6, 8, 10]      //the same output but with map we dont have to assign the values sepreatly
+    // [2, 4, 6, 8, 10]      //the same output as forEach but with map we dont have to assign the values sepreatly
 
-    
