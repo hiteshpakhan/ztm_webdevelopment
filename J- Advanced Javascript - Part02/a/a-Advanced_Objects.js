ca949263a888d82c96237e15723c6f066ee58800
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 16:52:42 +0530

    a

--- J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
+++ J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
@@ -1,3 +1,11 @@
 // reference type
+
+const object1 = { value: 10 };  //object is created
+const object2 = object1;        //
+const object3 = object2; 
+const object4 = { value: 10};  
+
+
+
 // contact type
 // instantiation
