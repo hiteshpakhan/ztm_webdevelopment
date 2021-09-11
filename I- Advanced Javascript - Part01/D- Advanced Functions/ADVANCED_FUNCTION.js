8bb88a2c4f18e87f163a1481ced3652adbd9c90a
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 06:22:00 +0530

    a

--- I- Advanced Javascript - Part01/D- Advanced Functions/ADVANCED_FUNCTION.js
+++ I- Advanced Javascript - Part01/D- Advanced Functions/ADVANCED_FUNCTION.js
@@ -1,5 +1,5 @@
-this part can blow your mind so stay focus
-# ADVANCED FUNCTION (CLOSURES) 
+// this part can blow your mind so stay focus
+// # ADVANCED FUNCTION (CLOSURES) 
 
     function first(){
         var greet = "hi";
@@ -11,9 +11,9 @@ this part can blow your mind so stay focus
     var new_fun = first();
     new_fun();
 
-* if we had to convert this example through the arrow function
+// * if we had to convert this example through the arrow function
 
-the conversion will be:-
+// the conversion will be:-
 
     const first = () => {
         const greet = "hi";
@@ -25,17 +25,17 @@ the conversion will be:-
     const new_fun = first();
     new_fun();                 
 
-------------------
+// ------------------
 
-# imp CURRYING
-* currying is the process of converting a function that takes multiple arguments into a function that takes them one at a time   
-* in short we are converting the function that takes multiple value into the function that takes single value 
+// # imp CURRYING
+// * currying is the process of converting a function that takes multiple arguments into a function that takes them one at a time   
+// * in short we are converting the function that takes multiple value into the function that takes single value 
 
-normal function:-
+// normal function:-
 
     const multiply = (a,b) => a * b;
 
-converted intocurrying function:-
+// converted intocurrying function:-
 
     const cm = (a) => (b) => a * b;      
     const mul = cm(3);        //here the three is the value of a
@@ -43,26 +43,26 @@ converted intocurrying function:-
     mul(10);
     mul(62);
 
-    output:-
-    12
-    30
-    186
+//     output:-
+//     12
+//     30
+//     186
 
-* now we have the function that always multiply the given value by 3
-* you can use this when you have to repit specific execution
--------------------
+// * now we have the function that always multiply the given value by 3
+// * you can use this when you have to repit specific execution
+// -------------------
 
-# COMPOSE
-act of putting of two function together to form a third function where the output of one function is the input of other
+// # COMPOSE
+// act of putting of two function together to form a third function where the output of one function is the input of other
 
     const compose = (f, g) => (a) => f(g(a));  //f and g are the function here f(g(a)) = sum(sum(7/11)) 
     const sum = (num) => num+1;                //sum = (5/11) => 5/11 + 1;
     compose(sum, sum)(5);                      //it will give the a=5, f=sum, g=sum in upper statement
     compose(sum, sum)(11);                     
 
-    output:-
-    7
-    13
+    // output:-
+    // 7
+    // 13
 
 
 
