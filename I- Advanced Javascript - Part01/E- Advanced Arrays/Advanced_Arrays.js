549bd2f074b7da7b2d1dcabcbbeeb1db7ae6d6d9
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 06:42:05 +0530

    a

--- I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
+++ I- Advanced Javascript - Part01/E- Advanced Arrays/Advanced_Arrays.js
@@ -8,3 +8,18 @@
 
     // output:-
     // [2, 4, 6, 8, 10]
+
+
+// # map, filter, reduce
+// these are the most important one
+
+//map
+
+    const arr = [1,2,3,4,5];
+    const newarr = arr.map((num) => {
+        return num * 2;
+    })
+    console.log(newarr);
+
+    // output:-
+    // [2, 4, 6, 8, 10]      //the output is the same but with map we dont have to assign the values sepreatly
