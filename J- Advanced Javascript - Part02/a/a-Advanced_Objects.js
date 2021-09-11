4ede407605a290fcc3ca2f8a3735d31f4a419366
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-11 17:54:20 +0530

    a

--- J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
+++ J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
@@ -2,9 +2,16 @@
 
 const object1 = { value: 10 };  //object is created
 const object2 = object1;        // here the main thing is
-//object2 got not assign the values of the boject1 its just referes the values of objectsss
-const object3 = object2;        //object3 got refer values of the object2
-const object4 = { value: 10};  
+                                //object2 got not assign the values of the boject1 its just referes the values of objectsss
+                                //object2 got refer values of the object1
+const object3 = { value: 10};  
+
+object1 === object2;  //true
+object1 === object3;  //false
+object1.value = 15;
+object2.value;        //15
+object3.value;        //10
+
 
 
 // contact type
