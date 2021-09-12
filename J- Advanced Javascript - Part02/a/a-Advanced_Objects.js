6a8116d6431442d8b582bcb7205e2728f288846c
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-12 07:34:27 +0530

    a

--- J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
+++ J- Advanced Javascript - Part02/a/a-Advanced_Objects.js
@@ -1,4 +1,4 @@
-// reference type
+// # reference type
 
 const object1 = { value: 10 };  //object is created
 const object2 = object1;        // here the main thing is
@@ -11,8 +11,34 @@ object1 === object3;  //false
 object1.value = 15;
 object2.value;        //15
 object3.value;        //10
+// -------------------------------------
+
+// # contact        //contact tels you which object are we in with the use of *"this"* keyword
+
+    
+    console.log(this);  
+    //op:- Window {0: global, window: Window, self: Window, document: document, name: '', location: Location, …}
+    //it will give you the location 
+    //basically if you are not in any object it will show you the window object because all the page made inside window object
+    
+    console.log(this === window);
+    // op:- true
+
+    //if you do "window.alert()" or you do "this.alert()" both are the same
+
+    const a = () => {
+        console.log(this);
+    }
+    a();
+    // op:-Window {0: global, window: Window, self: Window, document: document, name: '', location: Location, …}
+
+    const o = {
+        a: function(){
+            console.log(this);
+        }
+    };
+    o.a();
+    // op:- {a: ƒ}       //now as you can see as we are in the second object it shows the loaction of the second object that we are in 
 
 
-
-// contact type
-// instantiation
+// # instantiation type
