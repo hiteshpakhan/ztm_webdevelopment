5221481f85a7768d3c57bc69630e533690a98d16
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-09-14 14:36:10 +0530

    a

---
+++ J- Advanced Javascript - Part02/c- es7/a_es7.js
@@ -0,0 +1,32 @@
+// 1
+
+"hellooooo".includes('o');
+// op- true
+
+const pets = ["cat", "dog", "bat"];
+pets.includes("dog");
+// op- true
+
+pets.includes("fish");
+// op- false
+
+// ---------------------------------------------------
+
+// 2 exponentional operator
+
+const sq = (x) => x**2;    //it will make square calculator function
+sq(2);
+// op- 4
+sq(5);
+// op- 25
+
+
+const cu = (y) => y**3;    //it will make cube calculator function
+cu(4);
+// op- 64
+cu(675);
+// op- 307546875
+
+
+
+
