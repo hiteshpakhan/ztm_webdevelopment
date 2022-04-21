a0712ed974157a0284fb40e81123356e22cd64d1
Hitesh Pakhan <hitesh5pakhan@gmail.com>
2022-04-21 13:13:58 +0530

    a

--- M- node and express/part4 server/server.js
+++ M- node and express/part4 server/server.js
@@ -2,11 +2,17 @@ const http = require("http");
 
 const server = http.createServer((request, response) => {
     console.log("_______________________________");
-    // console.log("headers = ",request.headers);
+    console.log("headers = ",request.headers);
     console.log("method = ",request.method);
     console.log("url = ",request.url);
-    response.setHeader("content-type", "text/html");
-    response.end("<h1>hello</h1>");
+    // response.setHeader("content-type", "text/html");
+
+    const user = {
+        name : "john",
+        hobby : "skating" 
+    }
+    response.setHeader("content-type", "application/json");
+    response.end(JSON.stringify(user));
 })
 
 server.listen(3000);
